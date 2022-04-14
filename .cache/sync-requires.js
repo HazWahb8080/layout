9de9dd67244da8d6e02c552b042351dbc5d650a4
HazWahb8080 <hazem.wahb100@gmail.com>
2022-04-14 17:06:34 +0200

    initial and final

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\Lenovo\\Desktop\\shared-layout-animation-framer-motion\\12.framer-motion-shared-layout\\.cache\\dev-404-page.js")),
+  "component---src-components-model-index-tsx": preferDefault(require("C:\\Users\\Lenovo\\Desktop\\shared-layout-animation-framer-motion\\12.framer-motion-shared-layout\\src\\components\\Model\\index.tsx")),
+  "component---src-pages-index-jsx": preferDefault(require("C:\\Users\\Lenovo\\Desktop\\shared-layout-animation-framer-motion\\12.framer-motion-shared-layout\\src\\pages\\index.jsx"))
+}
+
